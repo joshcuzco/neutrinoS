a6a0628a7c0e5707ea1f003dcc7a31a1cd11b588
Josh Cuzco <jcr131998@gmail.com>
2020-08-16 09:43:17 -0600

    Added an easy example.

--- neutrinoS.py
+++ neutrinoS.py
@@ -1,4 +1,5 @@
 import numpy as np
+import MultiLayerPropagation as MLP
 
 """
 Simulation main program.
@@ -12,5 +13,9 @@ class neutrino:
 		#defined by its energy [eV] and zenith angle[degrees]
 		self.E=E
 		#converted to radian
-		self.angle=angle*np.pi/180
+		self.angle=a*np.pi/180
 
+#example
+n=neutrino(1,15)
+St=MLP.Propagate(n)
+print(abs(St[0]),abs(St[1]))
